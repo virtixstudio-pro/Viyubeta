8578394c15775324f2a877f9d1c77716c17c5954
Persévérance <ton_email_github@gmail.com>
2026-07-01 02:21:41 +0300

    Fix: v1.0.5.4 - Nettoyage du caractere parasite chinois dans beforeTextChanged

--- app/src/main/java/com/virtixstudio/viyubeta/AddContactActivity.java
+++ app/src/main/java/com/virtixstudio/viyubeta/AddContactActivity.java
@@ -52,7 +52,7 @@ public class AddContactActivity extends AppCompatActivity {
 
         etSearch.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int打) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
